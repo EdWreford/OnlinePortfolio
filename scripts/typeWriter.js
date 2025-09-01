7504391e614e489ca8b5df0b5e671557a10c650e
EdWreford <4ewreford@gmail.com>
2025-09-01 16:37:29 +0100

    Fixed typewriter.js, added some new images, updated Vetra project container, added details about Vetra

--- scripts/typeWriter.js
+++ scripts/typeWriter.js
@@ -1,23 +1,30 @@
 const typeWriter = (element, text, speed = 50) => {
-        return new Promise((resolve) => {
-            let i = 0;
-            const interval = setInterval(() => {
-            element.textContent += text.charAt(i);
-            i++;
-            if (i === text.length) {
-                clearInterval(interval);
-                resolve();
-            }
-            }, speed);
-        });
-        };
+  return new Promise((resolve) => {
+    let i = 0;
+    element.textContent = "";
+    const interval = setInterval(() => {
+      element.textContent += text.charAt(i);
+      i++;
+      if (i === text.length) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, speed);
+  });
+};
 
-        async function runSequence() {
-            const title = document.getElementById('bigTitle');
-            const subtitle = document.getElementById('categoryTitle');
+async function runSequence() {
+  const title = document.getElementById("bigTitle");
+  const subtitle = document.getElementById("categoryTitle");
 
-            await typeWriter(title, "Project Title", 75);
-            await typeWriter(subtitle, "Subtitle", 50);
-        }
+  const titleText = title.textContent;
+  const subtitleText = subtitle.textContent;
 
-        runSequence();
+  title.textContent = "";
+  subtitle.textContent = "";
+
+  await typeWriter(title, titleText, 75);
+  await typeWriter(subtitle, subtitleText, 50);
+}
+
+runSequence();
